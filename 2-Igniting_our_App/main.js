e461b0fd499f3a4a4670ade62c7429c29febaaf4
alokMahapatra26 <alokmahapatra2604@gmail.com>
2024-05-15 22:09:53 +0530

    minor changes

--- 2-Igniting_our_App/main.js
+++ 2-Igniting_our_App/main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 
-const parent = React.createElement("div" , {id : "parent"} , React.createElement("div" , {id : "child"} , [ React.createElement("h1" , {} , "hello world 1 hello cello") , React.createElement("h1" , {} , "hello world 2"), ]))
+const parent = React.createElement("div" , {id : "parent"} , React.createElement("div" , {id : "child"} , [ React.createElement("h1" , {} , "hello") , React.createElement("h1" , {} , "hello worldd"), ]))
 
 // const heading = React.createElement("h1" , {style : ""} , "hello world")
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
+
+
